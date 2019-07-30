99c20f89bcd099813c18289fef37f68163a7eafa
zhaikui89 <13804089948@163.com>
2019-07-30 15:11:15 +0800

    init

---
+++ server.js
@@ -0,0 +1,17 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const next = require('next')
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+const router = new Router()
+app.prepare().then(() => {
+  const server = new Koa()
+  server.use(async (ctx, next) => {
+      await handle(ctx.req, ctx.res)
+      ctx.respond= false
+  })
+  server.listen(3001, () => {
+      console.log(123)
+  })
+})
